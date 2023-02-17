0ac2fd79a7b9a5561895a5e8c4e033be9e01ff6c
Vikram S <vikram.samant@gmail.com>
2023-02-16 20:59:06 -0500

    Adding login/logout/signup functionality

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,11 +15,16 @@ urlpatterns = [
     # path for contact us view
     path(route='contact', view=views.contact, name='contact'),
 
+    path(route='navbar', view=views.navbar, name='navbar'),
+
     # path for registration
+    path('registration/', views.registration_request, name='registration'),
 
     # path for login
+    path(route='login/', view=views.login_request, name='login'),
 
     # path for logout
+    path(route='logout/', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
