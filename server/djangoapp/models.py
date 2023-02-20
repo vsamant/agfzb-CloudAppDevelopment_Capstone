09d7a419ea2a42b04a116d60064bd1c0d2131c5e
Vikram S <vikram.samant@gmail.com>
2023-02-19 20:02:09 -0500

    Fetching dealers from CF

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -54,6 +54,31 @@ class CarModel(models.Model):
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
 
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip, state):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state short
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+        # Dealer state
+        self.state = state
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+from .models import CarDealer
+from .restapis import get_dealers_from_cf, get_dealer_by_id, get_dealer_by_state
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -102,12 +102,20 @@ def registration_request(request):
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
-
+        url = "https://us-south.functions.appdomain.cloud/api/v1/web/ccde7b60-0223-47b0-b34a-f12b6ebf215e/api/dealership.json"
+        # Get dealers from the URL
+        #dealerships = get_dealers_from_cf(url)
+        #dealerships = get_dealer_by_id(url, 15)
+        dealerships = get_dealer_by_state(url, 'Texas')
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
 # ...
+
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
